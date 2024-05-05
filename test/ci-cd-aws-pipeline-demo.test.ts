9f52cf3cc7ee8ceb9a9d752bd39e26aaff3affd0
Abhi Patel <apate853@my.centennialcollege.ca>
2024-05-05 00:10:47 -0400

    updated code

--- test/ci-cd-aws-pipeline-demo.test.ts
+++ test/ci-cd-aws-pipeline-demo.test.ts
@@ -5,7 +5,3 @@
 import { handler } from "../lib/lambda/handler";
 
 //https://docs.aws.amazon.com/codebuild/latest/userguide/test-reporting.html
-test('foo', async () => {
-    const result = await handler("", "");
-    expect(result.statusCode).toEqual(300);
-});
