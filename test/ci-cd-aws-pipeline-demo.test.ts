967a4e412a11fafbd82e07907655756017e667a2
Abhi Patel <apate853@my.centennialcollege.ca>
2024-05-05 00:18:22 -0400

    updated code

--- test/ci-cd-aws-pipeline-demo.test.ts
+++
@@ -1,7 +0,0 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CiCdAwsPipelineDemo from '../lib/ci-cd-aws-pipeline-demo-stack';
-
-import { handler } from "../lib/lambda/handler";
-
-//https://docs.aws.amazon.com/codebuild/latest/userguide/test-reporting.html
